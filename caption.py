ce9d6b368c4dda5ee4ba9523f41f43e64cb59b66
HarishChandran3304 <harish3304.work@gmail.com>
2024-10-02 10:00:02 +0530

    resolved str nonetype error in caption.py

--- caption.py
+++ caption.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
-headers = {"Authorization": "Bearer " + os.getenv("HF_API_KEY")}
+headers = {"Authorization": "Bearer " + str(os.getenv("HF_API_KEY"))}
 
 def caption_img(image_data):
     response = requests.post(API_URL, headers=headers, data=image_data)
